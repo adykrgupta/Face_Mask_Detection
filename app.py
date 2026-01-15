06bb033fa3e858b0a2824aafd16dcbb70a1427e1
Aditya Kumar <AdyKr@users.noreply.huggingface.co>
2026-01-15 16:31:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,20 @@ import gradio as gr
 import tensorflow as tf
 import numpy as np
 
-# 1. Load your model
 model = tf.keras.models.load_model("mask_model.keras")
 class_names = ['with_mask', 'without_mask']
 
 def predict(img):
-    # Preprocess image to match EfficientNetV2B0 input (224x224)
+
     img = tf.image.resize(img, (224, 224))
-    img = tf.expand_dims(img, axis=0) # Add batch dimension
+    img = tf.expand_dims(img, axis=0) 
     
-    # Get prediction
     prediction = model.predict(img)[0][0]
-    
-    # Logic for Sigmoid output
     if prediction > 0.5:
         return {"Without Mask": float(prediction), "With Mask": float(1-prediction)}
     else:
         return {"With Mask": float(1-prediction), "Without Mask": float(prediction)}
 
-# 2. Create Gradio Interface
 interface = gr.Interface(
     fn=predict,
     inputs=gr.Image(),
